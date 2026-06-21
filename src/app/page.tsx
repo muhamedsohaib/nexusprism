11690f3e68d47afa3130171ca3a8c735f623c064
muhamedsohaib <muhammadsohaibsaif@gmail.com>
2026-06-21 22:08:12 +0400

    Use high resolution logo source on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 import { CinematicShaderHomepage } from '@/components/cinematic-shader-homepage'
-
-const markSrc = '/' + 'nexus-prism-logo-actual.svg'
+import { nexusPrismLogoSrc } from '@/lib/logo-data'
 
 export default function HomePage() {
   return (
     <>
       <div className="chrome-prism-core" aria-hidden="true">
-        <img src={markSrc} alt="" style={{ width: '100%', height: '100%', objectFit: 'contain' }} />
+        <img src={nexusPrismLogoSrc} alt="" style={{ width: '100%', height: '100%', objectFit: 'contain' }} />
       </div>
       <CinematicShaderHomepage />
     </>
