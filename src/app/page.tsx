d16f214cee591a91c37a88296f0a7d9b2649aec4
muhamedsohaib <muhammadsohaibsaif@gmail.com>
2026-06-21 22:15:21 +0400

    Add homepage logo layer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
 import { CinematicShaderHomepage } from '@/components/cinematic-shader-homepage'
 
 export default function HomePage() {
-  return <CinematicShaderHomepage />
+  const src = '/nexus-prism-logo-actual.svg'
+  return (
+    <>
+      <div className="chrome-prism-core" aria-hidden="true">
+        <img src={src} alt="" />
+      </div>
+      <CinematicShaderHomepage />
+    </>
+  )
 }
