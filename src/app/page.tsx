93db88d536aee0e1d795599550c97210b993f030
muhamedsohaib <muhammadsohaibsaif@gmail.com>
2026-06-21 22:14:38 +0400

    Remove broken homepage logo image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,5 @@
 import { CinematicShaderHomepage } from '@/components/cinematic-shader-homepage'
-import { nexusPrismLogoSrc } from '@/lib/logo-data'
 
 export default function HomePage() {
-  return (
-    <>
-      <div className="chrome-prism-core" aria-hidden="true">
-        <img src={nexusPrismLogoSrc} alt="" style={{ width: '100%', height: '100%', objectFit: 'contain' }} />
-      </div>
-      <CinematicShaderHomepage />
-    </>
-  )
+  return <CinematicShaderHomepage />
 }
