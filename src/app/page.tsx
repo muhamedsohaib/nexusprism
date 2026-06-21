f186144a53cec24f9ab41a8c2ddf30cae26e806d
muhamedsohaib <muhammadsohaibsaif@gmail.com>
2026-06-21 22:42:37 +0400

    Point homepage logo to uploaded PNG

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 import { CinematicShaderHomepage } from '@/components/cinematic-shader-homepage'
 
 export default function HomePage() {
-  const src = '/nexus-prism-logo-actual.svg'
   return (
     <>
       <div className="chrome-prism-core" aria-hidden="true">
-        <img src={src} alt="" />
+        <img src="/nexus-prism-logo.png" alt="" />
       </div>
       <CinematicShaderHomepage />
     </>
