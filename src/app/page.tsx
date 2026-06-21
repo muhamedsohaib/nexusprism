ea1185d26e17bf8c7060dc1a30c41f0a5d62879c
muhamedsohaib <muhammadsohaibsaif@gmail.com>
2026-06-21 22:05:20 +0400

    Show provided logo on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
 import { CinematicShaderHomepage } from '@/components/cinematic-shader-homepage'
 
+const markSrc = '/' + 'nexus-prism-logo-actual.svg'
+
 export default function HomePage() {
-  return <CinematicShaderHomepage />
+  return (
+    <>
+      <div className="chrome-prism-core" aria-hidden="true">
+        <img src={markSrc} alt="" style={{ width: '100%', height: '100%', objectFit: 'contain' }} />
+      </div>
+      <CinematicShaderHomepage />
+    </>
+  )
 }
